4803e8f5187cd86bb960814cc82c7af2314a1a0e
PruttinuShane <156614994+PruttinuShane@users.noreply.github.com>
2025-02-05 00:25:48 +0200

    2-4 finished

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const jwt = require ("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const SECRET_KEY = "1234";
+const user = [];
+const Highscore = [];
+
+
 
 app.use(express.json()); // for parsing application/json
 
 // ------ WRITE YOUR SOLUTION HERE BELOW ------//
 
-// Your solution should be written here
+app.post('/signup', async (req, res ) => {
+  const {email, password } = req.body;
+  const userExist = users.find(user => user.email ===email)
+  if (userExist) {
+    return res.status(400).json({message: 'User already exist'})
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = {email, password: hashedPassword };
+})
 
 //------ WRITE YOUR SOLUTION ABOVE THIS LINE ------//
 
